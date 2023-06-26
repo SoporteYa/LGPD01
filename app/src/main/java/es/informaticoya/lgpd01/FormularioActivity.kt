15f248aaa0252073ae978c28821c7f7809d6fed4
SoporteYa <133646077+SoporteYa@users.noreply.github.com>
2023-06-26 10:16:58 +0200

    Add files via upload

--- app/src/main/java/es/informaticoya/lgpd01/FormularioActivity.kt
+++ app/src/main/java/es/informaticoya/lgpd01/FormularioActivity.kt
@@ -1,87 +1,114 @@
 package es.informaticoya.lgpd01
 
-
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.TextView
+import com.google.android.play.core.integrity.i
+import es.informaticoya.lgpd01.databinding.ActivityFormularioBinding
 
 class FormularioActivity : AppCompatActivity() {
 
-    private lateinit var db: FirebaseFirestore
-    private lateinit var preguntaList: ArrayList<Pregunta>
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var myAdapter: MyAdapterPreguntas
+    private lateinit var binding: ActivityFormularioBinding
+    private lateinit var metodoRespuestaSpinner: Spinner
+    private lateinit var layoutRespuestas: LinearLayout
+    private val preguntasRespuestas = mutableListOf<PreguntaRespuesta>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario)
+        binding = ActivityFormularioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        db = FirebaseFirestore.getInstance()
-        preguntaList = ArrayList()
+        metodoRespuestaSpinner = findViewById(R.id.spinnerMetodoRespuesta)
+        layoutRespuestas = findViewById(R.id.layoutRespuestas)
 
-        recyclerView = findViewById(R.id.recyclerViewFormulario)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.btnAgregarRespuesta.setOnClickListener { view ->
+            agregarRespuesta(view)
+        }
 
-        myAdapter = MyAdapterPreguntas(preguntaList, this)
-        recyclerView.adapter = myAdapter
-
-        mostrarPreguntasDesdeDB()
-
-        val btnSaveFormulario: Button = findViewById(R.id.btnSaveFormulario)
-        btnSaveFormulario.setOnClickListener {
-            guardarFormulario()
+        binding.btnGuardarPregunta.setOnClickListener { view ->
+            guardarPregunta(view)
         }
     }
 
-    private fun mostrarPreguntasDesdeDB() {
-        db.collection("preguntas")
-            .get()
-            .addOnSuccessListener { querySnapshot ->
-                preguntaList.clear()
+    fun agregarRespuesta(view: View) {
+        val preguntaEditText = findViewById<EditText>(R.id.editTextPregunta)
+        val pregunta = preguntaEditText.text.toString()
 
-                for (document in querySnapshot) {
-                    val pregunta = document.toObject(Pregunta::class.java)
-                    preguntaList.add(pregunta)
-                }
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupRespuestas)
 
-                myAdapter.notifyDataSetChanged()
-            }
-            .addOnFailureListener { e ->
-                // Manejar el error de acuerdo a tus necesidades
-            }
+        val radioButton = RadioButton(this)
+        radioButton.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        radioButton.text = ""
+
+        val respuestaEditText = EditText(this)
+        respuestaEditText.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+
+        val radioButtonLayout = LinearLayout(this)
+        radioButtonLayout.orientation = LinearLayout.HORIZONTAL
+        radioButtonLayout.addView(radioButton)
+        radioButtonLayout.addView(respuestaEditText)
+
+        radioGroup.addView(radioButtonLayout)
+
+        preguntasRespuestas.add(PreguntaRespuesta(pregunta, listOf(respuestaEditText.text.toString())))
     }
 
-    private fun guardarFormulario() {
+    fun guardarPregunta(view: View) {
+        val preguntaEditText = findViewById<EditText>(R.id.editTextPregunta)
+        val pregunta = preguntaEditText.text.toString()
 
-        preguntaList.clear() // Limpiamos la lista actual de preguntas
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupRespuestas)
 
-        // Agregamos las preguntas predefinidas
-        preguntaList.add(Pregunta("Pregunta 1", listOf("Opción 1", "Opción 2", "Opción 3", "Opción 4")))
-        preguntaList.add(Pregunta("Pregunta 2", listOf("Opción A", "Opción B", "Opción C", "Opción D")))
-        preguntaList.add(Pregunta("Pregunta 3", listOf("Opción X", "Opción Y", "Opción Z")))
-        preguntaList.add(Pregunta("Pregunta 4", listOf("Opción 1", "Opción 2", "Opción 3")))
-        preguntaList.add(Pregunta("Pregunta 5", listOf("Opción A", "Opción B", "Opción C")))
+        val respuestas = mutableListOf<String>()
 
-
-        for (pregunta in preguntaList) {
-            db.collection("preguntas")
-                .add(pregunta)
-                .addOnSuccessListener { documentReference ->
-                    // La pregunta se guardó exitosamente en la base de datos
-                }
-                .addOnFailureListener { e ->
-                    // Hubo un error al guardar la pregunta en la base de datos
-                    // Maneja el error de acuerdo a tus necesidades
-                }
+        for (i in 0 until radioGroup.childCount) {
+            val radioButton = radioGroup.getChildAt(i) as RadioButton
+            val respuesta = radioButton.text.toString()
+            respuestas.add(respuesta)
         }
+
+        preguntasRespuestas[preguntasRespuestas.size - 1] = PreguntaRespuesta(pregunta, respuestas)
+        mostrarFormulario()
+    }
+
+
+    private fun mostrarFormulario() {
+        layoutRespuestas.removeAllViews()
+
+        for (preguntaRespuesta in preguntasRespuestas) {
+            val preguntaTextView = TextView(this)
+            preguntaTextView.text = preguntaRespuesta.pregunta
+            layoutRespuestas.addView(preguntaTextView)
+
+            for (respuesta in preguntaRespuesta.respuestas) {
+                val respuestaTextView = TextView(this)
+                respuestaTextView.text = respuesta
+                layoutRespuestas.addView(respuestaTextView)
+            }
+        }
+    }
+
+    private fun obtenerNumeroRespuestas(): Int {
+        // Aquí puedes implementar la lógica para obtener el número de respuestas deseado, por ejemplo, mostrar un cuadro de diálogo para que el usuario ingrese la cantidad de respuestas o utilizar un EditText adicional en la actividad para que el usuario escriba la cantidad.
+        // Retorna el número de respuestas ingresado por el usuario.
+        return 0
     }
 }
+
+
 
 
 
