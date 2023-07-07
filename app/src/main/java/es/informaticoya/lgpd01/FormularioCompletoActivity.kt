42d67b95852824a2ba3a913f0629a2ed31611764
SoporteYa <133646077+SoporteYa@users.noreply.github.com>
2023-07-07 20:34:45 +0200

    Add files via upload

--- app/src/main/java/es/informaticoya/lgpd01/FormularioCompletoActivity.kt
+++ app/src/main/java/es/informaticoya/lgpd01/FormularioCompletoActivity.kt
@@ -1,22 +1,30 @@
 package es.informaticoya.lgpd01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import es.informaticoya.lgpd01.databinding.ActivityFormularioCompletoBinding
 
-class FormularioCompletoActivity : AppCompatActivity() {
+class FormularioCompletoActivity : AppCompatActivity(),
+    FormularioCompletoAdapter.EditarPreguntaRespuestaListener {
 
     private lateinit var binding: ActivityFormularioCompletoBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: FormularioCompletoAdapter
     private val db = FirebaseFirestore.getInstance()
     private val preguntaRespuestasList = ArrayList<PreguntaRespuesta>()
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +34,10 @@ class FormularioCompletoActivity : AppCompatActivity() {
         recyclerView = binding.rvFormulario
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        adapter = FormularioCompletoAdapter(emptyList())
+        adapter = FormularioCompletoAdapter(mutableListOf())
         recyclerView.adapter = adapter
+        adapter.setEditarPreguntaRespuestaListener(this)
         cargarPreguntasDesdeFirestore()
-
     }
 
     private fun cargarPreguntasDesdeFirestore() {
@@ -53,4 +61,52 @@ class FormularioCompletoActivity : AppCompatActivity() {
                 ).show()
             }
     }
+
+    override fun onEditarPreguntaRespuesta(preguntaRespuesta: PreguntaRespuesta) {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.editar_preguntas_respuestas, null)
+        val editTextPregunta = dialogView.findViewById<EditText>(R.id.editTextPregunta)
+        editTextPregunta.setText(preguntaRespuesta.pregunta)
+
+        val layoutRespuestas = dialogView.findViewById<LinearLayout>(R.id.layoutRespuestas)
+        val botonAgregarRespuesta = dialogView.findViewById<Button>(R.id.botonAgregarRespuesta)
+
+        // Agregar respuestas existentes
+        for (respuesta in preguntaRespuesta.respuestas) {
+            val respuestaLayout = LayoutInflater.from(this).inflate(R.layout.item_respuesta, null)
+            val checkBox = respuestaLayout.findViewById<CheckBox>(R.id.checkBox)
+            val editTextRespuesta = respuestaLayout.findViewById<EditText>(R.id.editTextRespuesta)
+            editTextRespuesta.setText(respuesta)
+            layoutRespuestas.addView(respuestaLayout)
+        }
+
+        // Agregar listener al botón "Agregar Respuesta"
+        botonAgregarRespuesta.setOnClickListener {
+            val respuestaLayout = LayoutInflater.from(this).inflate(R.layout.item_respuesta, null)
+            layoutRespuestas.addView(respuestaLayout)
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+            .setTitle("Editar pregunta y respuestas")
+            .setView(dialogView)
+            .setPositiveButton("Guardar") { _, _ ->
+                val nuevaPregunta = editTextPregunta.text.toString()
+                val nuevasRespuestas = mutableListOf<String>()
+                for (i in 0 until layoutRespuestas.childCount) {
+                    val respuestaLayout = layoutRespuestas.getChildAt(i)
+                    val editTextRespuesta =
+                        respuestaLayout.findViewById<EditText>(R.id.editTextRespuesta)
+                    val respuesta = editTextRespuesta.text.toString()
+                    if (respuesta.isNotBlank()) {
+                        nuevasRespuestas.add(respuesta)
+                    }
+                }
+                val nuevaPreguntaRespuesta = PreguntaRespuesta(nuevaPregunta, nuevasRespuestas)
+                adapter.actualizarPreguntaRespuesta(preguntaRespuesta, nuevaPreguntaRespuesta)
+            }
+            .setNegativeButton("Cancelar", null)
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
 }
+
--- app/src/main/java/es/informaticoya/lgpd01/FormularioCompletoAdapter.kt
+++ app/src/main/java/es/informaticoya/lgpd01/FormularioCompletoAdapter.kt
@@ -3,16 +3,25 @@ package es.informaticoya.lgpd01
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class FormularioCompletoAdapter(emptyList: List<Any>) : RecyclerView.Adapter<FormularioCompletoAdapter.MyViewHolder>() {
+class FormularioCompletoAdapter(private var preguntasRespuestas: MutableList<PreguntaRespuesta>) :
+    RecyclerView.Adapter<FormularioCompletoAdapter.MyViewHolder>() {
 
-    private var preguntasRespuestas: MutableList<PreguntaRespuesta> = mutableListOf()
+    interface EditarPreguntaRespuestaListener {
+        fun onEditarPreguntaRespuesta(preguntaRespuesta: PreguntaRespuesta)
+    }
+
+    private var editarPreguntaRespuestaListener: EditarPreguntaRespuestaListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -25,15 +34,48 @@ class FormularioCompletoAdapter(emptyList: List<Any>) : RecyclerView.Adapter<For
         holder.bind(preguntaRespuesta)
     }
 
+
     override fun getItemCount(): Int {
         return preguntasRespuestas.size
     }
 
+    fun actualizarLista(nuevaLista: MutableList<PreguntaRespuesta>) {
+        preguntasRespuestas.clear()
+        preguntasRespuestas.addAll(nuevaLista)
+        notifyDataSetChanged()
+    }
+
+    fun setEditarPreguntaRespuestaListener(listener: EditarPreguntaRespuestaListener) {
+        editarPreguntaRespuestaListener = listener
+    }
+
+
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val preguntaTextView: TextView = itemView.findViewById(R.id.preguntaTextView)
-        private val opcionesRadioGroup: RadioGroup = itemView.findViewById(R.id.radioGroupRespuestas)
+        private val opcionesRadioGroup: RadioGroup =
+            itemView.findViewById(R.id.radioGroupRespuestas)
         private val botonEditar: Button = itemView.findViewById(R.id.botonEditar)
         private val botonBorrar: Button = itemView.findViewById(R.id.botonBorrar)
+
+        init {
+            botonEditar.setOnClickListener {
+                // Cambiar el modo de edición al hacer clic en el botón de editar
+                val nuevaPregunta = preguntaTextView.text.toString()
+                val nuevasRespuestas = obtenerRespuestasDesdeRadioGroup(opcionesRadioGroup)
+                val nuevaPreguntaRespuesta = PreguntaRespuesta(nuevaPregunta, nuevasRespuestas)
+                val adapterPosition = bindingAdapterPosition
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    editarPreguntaRespuestaListener?.onEditarPreguntaRespuesta(
+                        nuevaPreguntaRespuesta
+                    )
+                }
+            }
+
+            botonBorrar.setOnClickListener {
+                // Acción para el botón "Borrar"
+                // Puedes implementar aquí la lógica para borrar la pregunta y respuestas
+            }
+        }
 
         fun bind(preguntaRespuesta: PreguntaRespuesta) {
             preguntaTextView.text = preguntaRespuesta.pregunta
@@ -46,21 +88,26 @@ class FormularioCompletoAdapter(emptyList: List<Any>) : RecyclerView.Adapter<For
                 radioButton.text = respuesta
                 opcionesRadioGroup.addView(radioButton)
             }
-
-            botonEditar.setOnClickListener {
-                // Acción para el botón "Editar"
-                // Puedes implementar aquí la lógica para editar la pregunta y respuestas
+        }
+    }
+    private fun obtenerRespuestasDesdeRadioGroup(radioGroup: RadioGroup): List<String> {
+        val respuestas = mutableListOf<String>()
+        for (i in 0 until radioGroup.childCount) {
+            val radioButton = radioGroup.getChildAt(i) as? RadioButton
+            if (radioButton != null && radioButton.isChecked) {
+                respuestas.add(radioButton.text.toString())
             }
+        }
+        return respuestas
+    }
 
-            botonBorrar.setOnClickListener {
-                // Acción para el botón "Borrar"
-                // Puedes implementar aquí la lógica para borrar la pregunta y respuestas
-            }
+    fun actualizarPreguntaRespuesta(preguntaRespuestaAntigua: PreguntaRespuesta, preguntaRespuestaNueva: PreguntaRespuesta) {
+        val posicion = preguntasRespuestas.indexOf(preguntaRespuestaAntigua)
+        if (posicion != -1) {
+            preguntasRespuestas[posicion] = preguntaRespuestaNueva
+            notifyItemChanged(posicion)
         }
     }
 
-    fun actualizarLista(nuevaLista: List<PreguntaRespuesta>) {
-        preguntasRespuestas = nuevaLista.toMutableList()
-        notifyDataSetChanged()
-    }
 }
+
